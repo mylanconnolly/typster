2f10be84bd8b7af410db985575497afdc7a1e68f
Mylan Connolly <mylan@mylan.io>
2025-10-06 10:30:42 -0400

    Bump version and update documentation

--- mix.exs
+++ mix.exs
@@ -1,12 +1,12 @@
 defmodule Typster.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
       app: :typster,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
