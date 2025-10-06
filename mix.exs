8397ea4d9c6556f5eb28ed286bbc3d8b37d075bd
Mylan Connolly <mylan@mylan.io>
2025-10-06 09:18:43 -0400

    Relax elixir version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Typster.MixProject do
     [
       app: :typster,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "Typster",
